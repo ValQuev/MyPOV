56ddec30ed493542b7edeca3fc6840bf2ed57bd3
ValQuev <valentin.quevit@gmail.com>
2015-09-16 20:21:05 +0200

    Use mapInstance instead of map in onMapReady(GoogleMap map)

--- app/src/main/java/fr/valquev/mypov/MainActivity.java
+++ app/src/main/java/fr/valquev/mypov/MainActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
